9e1e4e44c22179efc9f6e73b8c3df992237429ae
labib0x0hunter <labibfaisal450@gmail.com>
2026-02-01 19:37:30 +0600

    refactored buffer.h and added comments

--- src/ds/buffer.c
+++ src/ds/buffer.c
@@ -1,30 +1,30 @@
 #include"buffer.h"
 
 string_t new_string(char* str) {
-    // printf("new_string() str=%s\n", str);
     string_t tmp = {.len = 0, .data = NULL};
-    if (str == NULL) {
-        return tmp;
-    }
+    if (str == NULL) return tmp;
+
     int len = strlen(str);
     char* data = (char*) malloc(sizeof(char) * (len + 1));
-    if (data == NULL) {
-        return tmp;
-    }
+    if (data == NULL) return tmp;
+
     tmp.len = len;
     tmp.data = data;
     strncpy(tmp.data, str, sizeof(char) * len);
     tmp.data[len] = '\0';
+    
     return tmp;
 }
 
 string_t new_n_string(int len) {
     string_t tmp = {.len = 0, .data = NULL};
+
     char* data = (char*) malloc(sizeof(char) * (len + 1));
     if (data == NULL) {
         printf("[log] new_n_string()=malloc() failed\n");
         return tmp;
     }
+    
     tmp.data = data;
     tmp.len = len;
     tmp.data[len] = '\0';
@@ -36,43 +36,43 @@ void init_string(string_t* s) {
     *s = new_string("");
 }
 
-// a = a + b
-// b frees
 bool append_string(string_t* a, string_t* b) {
     if (a == NULL || b == NULL) return false;
     if (a->data == NULL || b->data == NULL) return false;
+    
     // what happens here ? we store the pointer in another variable, waht if realloc fails ?? ptr will be null, but a->data ?
     char* ptr = (char*) realloc(a->data, sizeof(char) * (a->len + b->len + 1));
-    if (ptr == NULL) {
-        return false;
-    }
+    if (ptr == NULL) return false;
+    
     strncpy(ptr + a->len, b->data, sizeof(char) * b->len);
     ptr[a->len + b->len] = '\0';
     a->data = ptr;
     a->len += b->len;
-    // printf("append_string() str=%s\n", a->data);
     free_string(*b);
+    
     return true;
 }
 
 bool append_string_cstr(string_t* a, char* b) {
     if (a == NULL || b == NULL) return false;
     if (a->data == NULL) return false;
+
     int b_len = strlen(b);
     char* ptr = (char*) realloc(a->data, sizeof(char) * (a->len + b_len + 1));
-    if (ptr == NULL) {
-        return false;
-    }
+    if (ptr == NULL) return false;
+
     strncpy(ptr + a->len, b, sizeof(char) * b_len);
     ptr[a->len + b_len] = '\0';
     a->data = ptr;
     a->len += b_len;
-    // printf("append_string() str=%s\n", a->data);
+
     return true;
 }
 
 string_t copy_string(string_t str) {
     string_t new_str = new_n_string(str.len);
+    if (new_str.data == NULL) return new_str;
+
     memcpy(new_str.data, str.data, str.len);
     return new_str;
 }
